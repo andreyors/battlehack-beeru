ca1ea72604a98d29ea68bc1babe593505fce5ae3
Andrey O <andrey.orsoev@gmail.com>
2015-06-20 17:52:24 +0000

    Twilio sdk

--- data/db/init.sql
+++ data/db/init.sql
@@ -1,3 +1,45 @@
 CREATE DATABASE battlehackberlin;
 
 USE battlehackberlin;
+
+CREATE TABLE customers (
+    id INT(9) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    company VARCHAR(255),
+    website VARCHAR(255),
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(15),
+    address TEXT,
+    zipcode VARCHAR(255),
+    is_active TINYINT(1) DEFAULT 1
+) ENGINE=InnoDB;
+
+CREATE TABLE payments (
+    id INT(9) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    token VARCHAR(40) NOT NULL,
+    customer_id INT(9) NOT NULL,
+    status_id INT(9) NOT NULL,
+    amount DECIMAL(10,2) NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE statuses (
+    id INT(9) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    is_active TINYINT(1) DEFAULT 0
+) ENGINE=InnoDB;
+
+INSERT INTO statuses (title, is_active)
+    VALUES
+    ('pending', 1),
+    ('paid', 1),
+    ('rejected', 1),
+    ('cancelled', 1),
+    ('refunded', 1);
+
+CREATE TABLE items (
+    id INT(9) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    payment_id INT(9),
+    title VARCHAR(255),
+    price DECIMAL(10,2)
+) ENGINE=InnoDB;
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 error_reporting(E_ALL);
 ini_set('display_errors', true);
 
@@ -56,20 +57,51 @@ $app->get('/', function() use ($app, $db) {
   $app->render('index/index.twig');
 });
 
-$app->get('/api/payment', 'API', function() use ($app, $db) {
+$app->post('/api/payment', 'API', function() use ($app, $db) {
+  $rawData = file_get_contents("php://input");
+  $json = json_decode($rawData, true);
+  
+  $customer = !empty($json['customer']) ? $json['customer'] : array();
+  $items = !empty($json['items']) ? $json['items'] : array();
+  
   $sql = "SELECT NOW()";
   $res = $db->query($sql);
   
   $cnt = $res ? $res->fetchColumn() : 0;
   
-  $token = md5('hello');
+  $token = md5('token');
   
   $app->render(200, ['token' => $token]);
 });
 
+$app->get('/api/payment', function() {
+    $data = array(
+      'customer' => array(
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+        'email' => 'john.doe@gmail.com',
+        'phone' => '+491751261257',
+        ),
+        'items' => array(
+          array(
+            'title' => 'Sony PlayStation 4',
+            'price' => '399',
+            'currency' => 'EUR',
+          ),
+          array(
+            'title' => 'Witcher 3',
+            'price' => '69',
+            'currency' => 'EUR',
+          ),
+        ),
+      );
+      
+      die(json_encode($data));
+});
 
-$app->get('/api/payment/status', 'API', function() use ($app, $db) {
-    $token = !empty($_GET['token']) ? subtsr($_GET['token'], 0, 32) : false;
+
+$app->post('/api/payment/status', 'API', function() use ($app, $db) {
+    $token = !empty($_POST['token']) ? subtsr($_POST['token'], 0, 32) : false;
     
     $status = 'rejected';
     if (!empty($token)) {
@@ -77,6 +109,35 @@ $app->get('/api/payment/status', 'API', function() use ($app, $db) {
     }
     
     $app->render(200, ['status' => $status]);
+});
+
+$app->get('/api/payment/status', function() {
+  $data = array(
+    'token' => md5('token')
+    );
+    
+    die(json_encode($data));
+});
+
+$app->get('/api/')
+
+$app->get('/api/short', 'API', function() use ($app) {
+  $url = !empty($_GET['url']) ? $_GET['url'] : '';
+
+  if (!empty($url)) {
+    
+    include WEBROOT . "libs/GoogleShortener.php";
+  
+    $gApi = new GoogleShortener(GOOGLE_API_KEY);
+    $short = $gApi->shorten($url);
+    
+    $result = array(
+      "longUrl" => $url,
+      "url" => $short,
+    );
+    
+    die(json_encode($result));
+  }
 });
 
 $app->run();
